1e00796d5454570745b45d4b68f4f3b7cedc4180
Josiah Samuel <josiahsams@gmail.com>
2018-03-10 16:44:26 +0530

    first commit

---
+++ docker-work/bootstrap.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ "$JUST_RUN" = "N" ]; then
+  echo switching node to version $NODE_VERSION
+  n $NODE_VERSION --quiet
+fi
+
+echo node version: `node --version`
+
+if [ "$JUST_RUN" = "N" ]; then
+  git clone $GIT_URL app
+fi
+ 
+cd app
+
+cd $APP_HOME
+echo Application Home: $APP_HOME
+
+if [ "$JUST_RUN" = "N" ]; then
+  npm install --production --silent
+fi
+
+if [ "$APP_STARTUP" = "" ]; then
+  npm run $NPM_SCRIPT
+else
+  node $APP_STARTUP
+fi
